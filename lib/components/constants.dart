4af2ef19f5be1fe167b4a611b0b3c34de15da91c
Dipesh Jaiswal <dipeshjaiwal98@gmail.com>
2024-04-30 00:19:18 +0545

    Flutter Chatbot app complete code.

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -2,4 +2,4 @@ import 'dart:ui';
 
 Color backGround_color = const Color(0xFF343541);
 Color appBar_color = const Color(0xFF444654);
-const String apiKey="sk-proj-gAD9TVt1xeRObY6j0x0wT3BlbkFJGBr1HAUkofejlF3dR37k";
+const String apiKey="";
